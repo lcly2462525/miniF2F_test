47942fbe34e046f3e984fa5de100e157c405be71
lcly2462525 <lcly2462525@sjtu.edu.cn>
2025-02-21 15:54:28 +0800

    Add files via upload

---
+++ minif2f_10.lean
@@ -0,0 +1,74 @@
+import mathlib
+open Nat
+
+theorem mathd_algebra_478
+  (b h v : ℝ)
+  (h₀ : 0 < b ∧ 0 < h ∧ 0 < v)
+  (h₁ : v = 1 / 3 * (b * h))
+  (h₂ : b = 30)
+  (h₃ : h = 13 / 2) :
+  v = 65 := sorry
+
+theorem numbertheory_4x3m7y3neq2003
+  (x y : ℤ) :
+  4 * x^3 - 7 * y^3 ≠ 2003 := sorry
+
+-- Error: Real.log
+-- theorem aime_1983_p1
+--   (x y z w : ℕ)
+--   (ht : 1 < x ∧ 1 < y ∧ 1 < z)
+--   (hw : 0 ≤ w)
+--   (h0 : Real.log w / Real.log x = 24)
+--   (h1 : Real.log w / Real.log y = 40)
+--   (h2 : Real.log w / Real.log (x * y * z) = 12):
+--   Real.log w / Real.log z = 60 := sorry
+
+theorem amc12_2001_p5 :
+  Finset.prod (Finset.filter (λ x => ¬ Even x) (Finset.range 10000)) (id : ℕ → ℕ) = (10000!) / ((2^5000) * 5000!) := sorry
+
+theorem mathd_algebra_141
+  (a b : ℝ)
+  (h₁ : (a * b)=180)
+  (h₂ : 2 * (a + b)=54) :
+  (a^2 + b^2) = 369 := sorry
+
+theorem mathd_numbertheory_3 :
+  (∑ x in Finset.range 10, ((x + 1)^2)) % 10 = 5 := sorry
+
+theorem imo_1969_p2
+  (m n : ℝ)
+  (k : ℕ)
+  (a : ℕ → ℝ)
+  (y : ℝ → ℝ)
+  (h₀ : 0 < k)
+  (h₁ : ∀ x, y x = ∑ i in Finset.range k, ((Real.cos (a i + x)) / (2^i)))
+  (h₂ : y m = 0)
+  (h₃ : y n = 0) :
+  ∃ t : ℤ, m - n = t * π := sorry
+
+theorem mathd_algebra_44
+  (s t : ℝ)
+  (h₀ : s = 9 - 2 * t)
+  (h₁ : t = 3 * s + 1) :
+  s = 1 ∧ t = 4 := sorry
+
+theorem mathd_algebra_209
+  (σ : Equiv ℝ ℝ)
+  (h₀ : σ.2 2 = 10)
+  (h₁ : σ.2 10 = 1)
+  (h₂ : σ.2 1 = 2) :
+  σ.1 (σ.1 10) = 1 := sorry
+
+theorem mathd_numbertheory_1124
+  (n : ℕ)
+  (h₀ : n ≤ 9)
+  (h₁ : 18∣374 * 10 + n) :
+  n = 4 := sorry
+
+theorem imo_1983_p6
+  (a b c : ℝ)
+  (h₀ : 0 < a ∧ 0 < b ∧ 0 < c)
+  (h₁ : c < a + b)
+  (h₂ : b < a + c)
+  (h₃ : a < b + c) :
+  0 ≤ a^2 * b * (a - b) + b^2 * c * (b - c) + c^2 * a * (c - a) := sorry
